c00e24744dc61d990787e4f117d159fa7969c5a8
tehuyoryu-cpu <tehuyoryu@gmail.com>
2026-05-29 19:45:20 +0930

    Fix: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  }
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
