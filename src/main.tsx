3c1b41fec4416d3aaabee41586f28345833bc15e
tehuyoryu-cpu <tehuyoryu@gmail.com>
2026-05-29 19:45:21 +0930

    Fix: src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const rootEl = document.getElementById('root')
+if (!rootEl) {
+  document.body.innerHTML = '<div style="color:red;padding:20px">root要素が見つかりません</div>'
+} else {
+  ReactDOM.createRoot(rootEl).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
